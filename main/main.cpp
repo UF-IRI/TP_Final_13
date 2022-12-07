781c015c8693629b7bb11a8a91b98b1982e2a085
Micaela Rivero Díaz <micaela.rivero.diaz@gmail.com>
2022-12-06 22:02:38 -0300

    path corregidos

--- main/main.cpp
+++ main/main.cpp
@@ -8,13 +8,13 @@
 int main() {
     int op;
     int pip;
-    string pathRetornantesConfirmados = R"(..\\..\\..\\..\\data_files\\output\\RetornantesConfirmados.csv)";
+    string pathRetornantesConfirmados = "..\\..\\..\\..\\data_files\\output\\RetornantesConfirmados.csv";
     op = filtro("..\\..\\..\\..\\data_files\\input\\Consultas.csv", "..\\..\\..\\..\\data_files\\input\\Pacientes.csv", "..\\..\\..\\..\\data_files\\input\\Contactos.csv");
 
     if (op == -1)
         cout << "Hay problema en filtro" << endl;
     inicializarArchivoRetornantes(pathRetornantesConfirmados);
-    pip = mainSecretria(R"(..\\..\\..\\..\\data_files\\output\\Retornantes.csv)",pathRetornantesConfirmados);
+    pip = mainSecretria("..\\..\\..\\..\\data_files\\output\\Retornantes.csv",pathRetornantesConfirmados);
 
     if (pip == -1)
         cout << "Hay problema en secretaria" << endl;
